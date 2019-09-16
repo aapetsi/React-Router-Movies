2070fc0350c2df40ace7bd726bf6af79c5ed6350
Apetsi Ampiah <apetsiampiah@gmail.com>
2019-09-16 12:57:53 +0000

    Refactor MovieList and MovieCard

---
+++ client/src/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navigation = () => {
+  return (
+    <div>
+      <Link to="/">Movies</Link>
+      {/* <Link></Link>
+      <Link></Link> */}
+    </div>
+  );
+};
+
+export default Navigation;
